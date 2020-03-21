828df0ecc4e5a9c3a9bb27cddb338d9215e7d8bc
himanshu khare <khareh57@gmail.com>
2020-03-21 15:19:05 +0530

    Banking app

---
+++ index.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const express = require("express");
+const expressEdge = require("express-edge");
+
+const app = new express();
+
+app.use(express.static("public"));
+app.use(expressEdge.engine);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/login.html"));
+});
+
+app.listen(4000, () => {
+  console.log("App listening on port 4000");
+});
